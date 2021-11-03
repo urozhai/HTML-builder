082cf4b60fcb71def684a324a7cf3e6b214496b4
Anton Kachan <aa.kvchvn@gmail.com>
2021-11-03 23:00:17 +0300

    feat: solve 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+const stdout = process.stdout;
 
+async function copyDir() {
+  try {
+    await fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
+    const files = await fsPromises.readdir(path.join(__dirname, 'files'));
+    for (let i = 0; i < files.length; i++) {
+      await fsPromises.copyFile(path.join(__dirname, 'files', files[i]), path.join(__dirname, 'files-copy', files[i]));
+    }
+    stdout.write('"files" copied to "files-copy"');
+  } catch (err) {
+    stdout.write(err.name + ': ' + err.message);
+  }
+}
+
+copyDir();
